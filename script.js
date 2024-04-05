927b238f838efd06a9e691567c5376a042b1b1b1
Paula Moheb <poulamoheb12@gmail.com>
2024-04-05 17:52:37 +0200

    Adding the Dom elements

---
+++ script.js
@@ -0,0 +1,31 @@
+// Variables 
+//   DOM elements
+const equationHolderText=document.querySelector("#equation-holder");
+const acText=document.querySelector("#ac");
+const plusMinusText=document.querySelector("#plus-minus");
+const percentText=document.querySelector("#percent");
+const divideText=document.querySelector("#divide");
+const sevenText=document.querySelector("#seven");
+const eightText=document.querySelector("#eight");
+const nineText=document.querySelector("#nine");
+const multiplyText=document.querySelector("#multiply");
+const fourText=document.querySelector("#four");
+const fiveText=document.querySelector("#five");
+const sixText=document.querySelector("#six");
+const subtractText=document.querySelector("#subtract");
+const oneText=document.querySelector("#one");
+const twoText=document.querySelector("#two");
+const threeText=document.querySelector("#three");
+const sumText=document.querySelector("#sum");
+const zeroText=document.querySelector("#zero");
+const dotText=document.querySelector("#dot");
+const equalText=document.querySelector("#equal");
+// event listener
+// event listener
+acText.addEventListener("click",()=>{
+    equationHolderText.textContent="0";
+});
+plusMinusText.addEventListener("click",()=>{
+});
+acText.addEventListener("click",()=>{
+});
