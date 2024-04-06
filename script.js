2b292f3602cb92e664647b250e9e6fe7549ab5b4
Paula Moheb <poulamoheb12@gmail.com>
2024-04-06 18:00:16 +0200

    Adding event lestiners for numbers ,variables and functions(reset,writingNumber)

--- script.js
+++ script.js
@@ -1,31 +1,76 @@
 // Variables 
+let total=0
+    ,equationNum=[0]
+    ,numberHolder=""
+    ,equationOperation=[];
+
 //   DOM elements
-const equationHolderText=document.querySelector("#equation-holder");
-const acText=document.querySelector("#ac");
-const plusMinusText=document.querySelector("#plus-minus");
-const percentText=document.querySelector("#percent");
-const divideText=document.querySelector("#divide");
-const sevenText=document.querySelector("#seven");
-const eightText=document.querySelector("#eight");
-const nineText=document.querySelector("#nine");
-const multiplyText=document.querySelector("#multiply");
-const fourText=document.querySelector("#four");
-const fiveText=document.querySelector("#five");
-const sixText=document.querySelector("#six");
-const subtractText=document.querySelector("#subtract");
-const oneText=document.querySelector("#one");
-const twoText=document.querySelector("#two");
-const threeText=document.querySelector("#three");
-const sumText=document.querySelector("#sum");
-const zeroText=document.querySelector("#zero");
-const dotText=document.querySelector("#dot");
-const equalText=document.querySelector("#equal");
-// event listener
+const equationHolderText=document.querySelector("#equation-holder")
+     ,acText=document.querySelector("#ac")
+     ,plusMinusText=document.querySelector("#plus-minus")
+     ,percentText=document.querySelector("#percent")
+     ,divideText=document.querySelector("#divide")
+     ,sevenText=document.querySelector("#seven")
+     ,eightText=document.querySelector("#eight")
+     ,nineText=document.querySelector("#nine")
+     ,multiplyText=document.querySelector("#multiply")
+     ,fourText=document.querySelector("#four")
+     ,fiveText=document.querySelector("#five")
+     ,sixText=document.querySelector("#six")
+     ,subtractText=document.querySelector("#subtract")
+     ,oneText=document.querySelector("#one")
+     ,twoText=document.querySelector("#two")
+     ,threeText=document.querySelector("#three")
+     ,sumText=document.querySelector("#sum")
+     ,zeroText=document.querySelector("#zero")
+     ,dotText=document.querySelector("#dot")
+     ,equalText=document.querySelector("#equal");
 // event listener
 acText.addEventListener("click",()=>{
-    equationHolderText.textContent="0";
+    reset();
 });
-plusMinusText.addEventListener("click",()=>{
+    // num 
+zeroText.addEventListener("click",()=>{
+    numberHolder.length>0?writingNumber(0):numberHolder="";
 });
-acText.addEventListener("click",()=>{
+oneText.addEventListener("click",()=>{
+    writingNumber(1);
 });
+twoText.addEventListener("click",()=>{
+    writingNumber(2);
+});
+threeText.addEventListener("click",()=>{
+    writingNumber(3);
+});
+fourText.addEventListener("click",()=>{
+    writingNumber(4);
+});
+fiveText.addEventListener("click",()=>{
+    writingNumber(5);
+});
+sixText.addEventListener("click",()=>{
+    writingNumber(6);
+});
+sevenText.addEventListener("click",()=>{
+    writingNumber(7);
+});
+eightText.addEventListener("click",()=>{
+    writingNumber(8);
+});
+nineText.addEventListener("click",()=>{
+    writingNumber(9);
+});
+    // operations
+
+// functions 
+function reset (){
+    total=0
+    ,equationNum=[0]
+    ,numberHolder=""
+    ,equationOperation=[]
+    ,equationHolderText.textContent="0";
+} 
+function writingNumber(numString){
+    numberHolder+=numString;
+    equationHolderText.textContent=numberHolder;
+}
